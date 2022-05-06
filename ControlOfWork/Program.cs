3a2cb98932705b8c95c5ba89b31e314d14f445f6
Alexey Viklov <alexey.viklov@gmail.com>
2022-05-06 13:59:06 +0300

    Project structure changed
    _Database  folder renamed to DB scripts
    _Application folder moved to repository root
    PL: Radzen.Blazor package added

--- ControlOfWork/Program.cs
+++ ControlOfWork/Program.cs
@@ -1,11 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
